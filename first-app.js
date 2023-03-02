6fa4f8f39c0d7d15a936ee7066a8a4815a303251
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-02 19:35:14 +0530

    variables, let, const, fat arrow func, and objects

--- first-app.js
+++ first-app.js
@@ -1,1 +1,20 @@
-console.log('Hello from Node.js');
+console.log('Hello World');
+
+
+// fat arrow function return product of two numbers
+const productNumbers = (a, b) => a * b;
+
+console.log(productNumbers(5, 6));
+
+// student object
+const student = {
+    name: 'Yansh',
+    age: 23,
+    specialization: 'Btech-CS',
+
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+
+student.greet();
