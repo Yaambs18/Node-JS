9dec374979935dc0220ed13df8ca54f18a684b4e
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-02 21:15:19 +0530

    arrays, objects, spread & rest operator, destructuring

--- first-app.js
+++ first-app.js
@@ -18,3 +18,47 @@ const student = {
 }
 
 student.greet();
+
+// ararays
+const hobbies = ['Sports', 'Cooking'];
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+console.log(hobbies.map(hobby => 'Hobby: '+hobby));
+console.log(hobbies);
+
+// -----------
+const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+const updatedArray = array.map(item => {
+    if(item === ' '){
+        return 'empty string';
+    }else{
+        return item;
+    }
+});
+
+console.log(updatedArray);
+
+// spread operator
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+// rest operator
+const toArray = (...args) => {
+    return args;
+}
+console.log(toArray(1,2,3,4));
+
+// destructuring
+
+// const printname = (studentData) => {
+//     console.log(studentData.name);
+// }
+
+const printName = ({ name }) => {
+    console.log(name);
+}
+printName(student);
+
+const {name, age} = student;
+console.log(name, age);
