480a1bc18bb9c328bdb624a0491a1f7840d9aa7e
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-03 16:01:43 +0530

    updated readFile method

--- app.js
+++ app.js
@@ -12,14 +12,21 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body>');
-        res.write(`<p>${fs.readFileSync('message.txt')}</p>`)
-        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
-        res.write('</body>');
-        res.write('</html>');
-        return res.end();
+
+        fs.readFile('message.txt', {encoding: 'utf-8'}, (err, data) => {
+            if(err){
+                console.log(err);
+            }
+            console.log('data from file:',data);
+            res.write('<html>');
+            res.write('<head><title>Enter Message</title></head>');
+            res.write('<body>');
+            res.write(`<p>${data}</p>`)
+            res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+            res.write('</body>');
+            res.write('</html>');
+            return res.end();
+        })
     }
     if(url === '/message' && method === 'POST'){
         const body = [];
