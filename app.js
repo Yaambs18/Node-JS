5a5aa1d80d76e7b351289f3eb3445310ebea8bf6
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-03 13:33:56 +0530

    working with requests, responses in node js

--- app.js
+++ app.js
@@ -5,8 +5,37 @@ const http = require('http');
 // function rqListener(req, res) {}
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    console.log('Yansh');
+    // console.log(req.url, req.method, req.headers);
+    // console.log('Yansh');
+    // process.exit();
+    const url = req.url;
+    if(url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<bodey><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url === '/about'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<bodey><h1>Welcome to About Us page</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(url === '/node'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<bodey><h1>Welcome to my Node Js project</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<bodey><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(4000);
