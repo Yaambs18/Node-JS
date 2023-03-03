ef71919df14d067e92b30947bb5ba83e3b79cdad
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-03 15:12:29 +0530

    Redirecting requests, parsing request bodies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // server creation
  
 const http = require('http');
+const fs = require('fs');
 
 // function rqListener(req, res) {}
 
@@ -9,7 +10,29 @@ const server = http.createServer((req, res) => {
     // console.log('Yansh');
     // process.exit();
     const url = req.url;
-    if(url === '/home'){
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<bodey><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) =>{
+            body.push(chunk);
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    else if(url === '/home'){
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
         res.write('<bodey><h1>Welcome home</h1></body>');
