a830476b1f8a30c03473f4ac303f491b47e75eba
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-03 15:48:16 +0530

    difference between fs,writeFile and writeFileSync (Blocking and non-blocking)

--- app.js
+++ app.js
@@ -26,37 +26,42 @@ const server = http.createServer((req, res) => {
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
+            // fs.writeFileSync('message.txt', message);
+            // res.statusCode = 302;
+            // res.setHeader('Location', '/');
+            // return res.end();
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         })
     }
     else if(url === '/home'){
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
-        res.write('<bodey><h1>Welcome home</h1></body>');
+        res.write('<body><h1>Welcome home</h1></body>');
         res.write('</html>');
         return res.end();
     }
     else if(url === '/about'){
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
-        res.write('<bodey><h1>Welcome to About Us page</h1></body>');
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
         res.write('</html>');
         return res.end();
     }
     else if(url === '/node'){
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
-        res.write('<bodey><h1>Welcome to my Node Js project</h1></body>');
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
         res.write('</html>');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
-    res.write('<bodey><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
 })
